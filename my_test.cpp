52d56ece4e0625c06938c1a224628f564ac1e78e
bds1984 <52322872+bds1984@users.noreply.github.com>
2019-07-17 11:13:32 +0300

    Update my_test.cpp

--- my_test.cpp
+++ my_test.cpp
@@ -10,6 +10,22 @@ BOOST_AUTO_TEST_CASE(test_split)
 {
 std::vector<std::string> v_test= split("1.1.1.1",'.');   
 BOOST_CHECK(v_test.size()==4);
+
+  type_storage pool;
+  std::vector<std::string> vct;
+  for(int i=0;i<5;++i){
+  vct.emplace_back(i);
+    vct.emplace_back(i);
+      vct.emplace_back(i);
+       vct.emplace_back(i);
+    pool.emplace_back(vct);
+    vct.resize(0);
+  }
+ BOOST_CHECK(pool.size()==5); 
 }
 
 }
+
+
+
+  
