3a6e8a9aaba426f001cb6ef642068ec0bdd4be05
bds1984 <52322872+bds1984@users.noreply.github.com>
2019-07-17 10:26:49 +0300

    Update my_test.cpp

--- my_test.cpp
+++ my_test.cpp
@@ -8,8 +8,7 @@ BOOST_AUTO_TEST_SUITE(my_test)
 
 BOOST_AUTO_TEST_CASE(test_1)
 {
-  std::string str="1.1.1.1";
-std::vector<std::string> v_test= split(str,'.');   
+std::vector<std::string> v_test= split("1.1.1.1",'.');   
 BOOST_CHECK(v_test.size()==4);
 }
 
