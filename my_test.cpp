6445b90e048d2bd4a24f6850bb811d6d2affb3ba
bds1984 <52322872+bds1984@users.noreply.github.com>
2019-07-17 10:57:20 +0300

    Update my_test.cpp

--- my_test.cpp
+++ my_test.cpp
@@ -12,25 +12,4 @@ std::vector<std::string> v_test= split("1.1.1.1",'.');
 BOOST_CHECK(v_test.size()==4);
 }
 
-BOOST_AUTO_TEST_CASE(test_loxograf)
-{
-  type_storage pool;
-  std::vector<std::string> vct;
-  for(int i=0;i<5;++i){
-  vct.emplace_back(i);
-    vct.emplace_back(i);
-      vct.emplace_back(i);
-       vct.emplace_back(i);
-    pool.emplace_back(vct);
-    vct.resize(0);
-  }
-lexografsort(pool);  
- int i{4}; 
-  for (const auto iter : pool){
-   BOOST_CHECK(std::stoi(*iter.cbegin())==i)  
-     --i;
-  }
-
-}
-
 }
