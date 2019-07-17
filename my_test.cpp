828afe4afe72414c98b72741de206bf230b9ba3c
bds1984 <52322872+bds1984@users.noreply.github.com>
2019-07-17 12:24:41 +0300

    Update my_test.cpp

--- my_test.cpp
+++ my_test.cpp
@@ -26,9 +26,12 @@ vct.resize(0);
   }
 lexografsort(pool); 
   vct.resize(0);
-  vct=pool[0];
-BOOST_CHECK(pool.size()>0);
- 
+  int x{4}
+for(int i=4;i>-1;=--1){
+ vct=pool[i];
+BOOST_CHECK(  vct[1]==x);
+  --x;
+} 
 
 
 }
